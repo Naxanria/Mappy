d52216ca8832d7ba94e64310ba66eb4a3a1e6fd8
James Mastros <james@mastros.biz>
2019-05-18 23:02:34 +0100

    First go at a terrain (height) shader layer.

--- src/main/java/com/naxanria/mappy/map/MapLayerProcessor.java
+++ src/main/java/com/naxanria/mappy/map/MapLayerProcessor.java
@@ -12,6 +12,47 @@ public class MapLayerProcessor
 {
   public static final int BLACK = 0xff000000;
   
+
+  // Returns an rgba color?
+  public static int shadeTopView(WorldChunk chunk, int x, int z)
+  {
+    //World world = chunk.getWorld();
+
+    if (x==15 || z==0) {
+      return 0;
+    }
+
+    Heightmap heightmap = chunk.getHeightmap(Heightmap.Type.MOTION_BLOCKING);
+    int y_here = heightmap.get(x, z);
+    int y_east = heightmap.get(x+1, z);
+    int y_south = heightmap.get(x, z-1);
+
+    // https://en.wikipedia.org/wiki/Terrain_cartography#Shaded_relief states that shading convention is that the light is from the top-left corner
+    // of the map.
+    int y_diff_east = y_east - y_here;
+    int y_diff_south = -(y_south - y_here);
+
+    int y_diff = y_diff_east + y_diff_south;
+
+    int base_color;
+    if (y_diff < 0) {
+      // Black
+      base_color = 0;
+      y_diff = -y_diff;
+    } else {
+      // White
+      base_color = 0xFFFFFF;
+    }
+    
+    if (y_diff > 8) {
+      y_diff = 8;
+    }
+
+    int alpha = (int)(255.0 * y_diff/8.0);
+
+    return (alpha << 24) | base_color;
+  }
+
   public static int processTopView(WorldChunk chunk, int x, int z)
   {
     World world = chunk.getWorld();
